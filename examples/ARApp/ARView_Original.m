4e9503b3a5d2169fa21f173aa5c8771a6cae7d17
Reed Tomlinson <dev1@carbonobjects.com>
2016-11-26 21:44:26 -0600

    Working example of reading shaders from files

--- examples/ARApp/ARView_Original.m
+++ examples/ARApp/ARView_Original.m
@@ -1,3 +1,51 @@
+//
+//  ARView.m
+//  ARApp
+//
+//  Disclaimer: IMPORTANT:  This Daqri software is supplied to you by Daqri
+//  LLC ("Daqri") in consideration of your agreement to the following
+//  terms, and your use, installation, modification or redistribution of
+//  this Daqri software constitutes acceptance of these terms.  If you do
+//  not agree with these terms, please do not use, install, modify or
+//  redistribute this Daqri software.
+//
+//  In consideration of your agreement to abide by the following terms, and
+//  subject to these terms, Daqri grants you a personal, non-exclusive
+//  license, under Daqri's copyrights in this original Daqri software (the
+//  "Daqri Software"), to use, reproduce, modify and redistribute the Daqri
+//  Software, with or without modifications, in source and/or binary forms;
+//  provided that if you redistribute the Daqri Software in its entirety and
+//  without modifications, you must retain this notice and the following
+//  text and disclaimers in all such redistributions of the Daqri Software.
+//  Neither the name, trademarks, service marks or logos of Daqri LLC may
+//  be used to endorse or promote products derived from the Daqri Software
+//  without specific prior written permission from Daqri.  Except as
+//  expressly stated in this notice, no other rights or licenses, express or
+//  implied, are granted by Daqri herein, including but not limited to any
+//  patent rights that may be infringed by your derivative works or by other
+//  works in which the Daqri Software may be incorporated.
+//
+//  The Daqri Software is provided by Daqri on an "AS IS" basis.  DAQRI
+//  MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+//  THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+//  FOR A PARTICULAR PURPOSE, REGARDING THE DAQRI SOFTWARE OR ITS USE AND
+//  OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+//
+//  IN NO EVENT SHALL DAQRI BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+//  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//  INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+//  MODIFICATION AND/OR DISTRIBUTION OF THE DAQRI SOFTWARE, HOWEVER CAUSED
+//  AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+//  STRICT LIABILITY OR OTHERWISE, EVEN IF DAQRI HAS BEEN ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+//  Copyright 2015 Daqri LLC. All Rights Reserved.
+//  Copyright 2010-2015 ARToolworks, Inc. All rights reserved.
+//
+//  Author(s): Philip Lamb
+//
+
 #import <QuartzCore/QuartzCore.h>
 #import "ARView.h"
 #import "ARViewController.h"
@@ -297,25 +345,46 @@ enum {
     if (!program) {
         GLuint vertShader = 0, fragShader = 0;
         // A simple shader pair which accepts just a vertex position and colour, no lighting.
-        const char vertShaderString[] =
-            "attribute vec4 position;\n"
-            "attribute vec4 colour;\n"
-            "uniform mat4 modelViewProjectionMatrix;\n"
-            "varying vec4 colourVarying;\n"
-            "void main()\n"
-            "{\n"
-                "gl_Position = modelViewProjectionMatrix * position;\n"
-                "colourVarying = colour;\n"
-            "}\n";
-        const char fragShaderString[] =
-            "#ifdef GL_ES\n"
-            "precision mediump float;\n"
-            "#endif\n"
-            "varying vec4 colourVarying;\n"
-            "void main()\n"
-            "{\n"
-                "gl_FragColor = colourVarying;\n"
-            "}\n";
+//        const char vertShaderString[] =
+//            "attribute vec4 position;\n"
+//            "attribute vec4 colour;\n"
+//            "uniform mat4 modelViewProjectionMatrix;\n"
+//            "varying vec4 colourVarying;\n"
+//            "void main()\n"
+//            "{\n"
+//                "gl_Position = modelViewProjectionMatrix * position;\n"
+//                "colourVarying = colour;\n"
+//            "}\n";
+//        const char fragShaderString[] =
+//            "#ifdef GL_ES\n"
+//            "precision mediump float;\n"
+//            "#endif\n"
+//            "varying vec4 colourVarying;\n"
+//            "void main()\n"
+//            "{\n"
+//                "gl_FragColor = colourVarying;\n"
+//            "}\n";
+        
+        
+        NSString *vertexShaderName = @"SimpleVertex_Original";
+        NSString* vertexShaderPath = [[NSBundle mainBundle] pathForResource:vertexShaderName ofType:@"glsl"];
+        NSError* error;
+        NSString* vertexShaderNSString = [NSString stringWithContentsOfFile:vertexShaderPath encoding:NSUTF8StringEncoding error:&error];
+        if (!vertexShaderNSString) {
+            NSLog(@"Error loading shader: %@", error.localizedDescription);
+            exit(1);
+        }
+        const char *vertexShaderString = [vertexShaderNSString cStringUsingEncoding:NSASCIIStringEncoding];
+        
+        NSString *fragmentShaderName = @"SimpleFragment_Original";
+        NSString* fragmentShaderPath = [[NSBundle mainBundle] pathForResource:fragmentShaderName ofType:@"glsl"];
+        NSString* fragmentShaderNSString = [NSString stringWithContentsOfFile:fragmentShaderPath encoding:NSUTF8StringEncoding error:&error];
+        if (!fragmentShaderNSString) {
+            NSLog(@"Error loading shader: %@", error.localizedDescription);
+            exit(1);
+        }
+        const char *fragmentShaderString = [fragmentShaderNSString cStringUsingEncoding:NSASCIIStringEncoding];
+
         
         if (program) arglGLDestroyShaders(0, 0, program);
         program = glCreateProgram();
